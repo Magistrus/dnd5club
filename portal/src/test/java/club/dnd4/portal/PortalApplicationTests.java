f7b8320b28480eceb4987a85e4d3fc567a649450
Виталий <@172.19.249.123>
2021-09-08 22:54:05 +0300

    -3

---
+++ portal/src/test/java/club/dnd4/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package club.dnd4.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
