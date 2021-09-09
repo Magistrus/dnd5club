8bd32001eca1221136f01538fda9ad8a46dd74cc
Peterko <peterkojob@gmail.com>
2021-09-09 20:09:02 +0300

    Настройка блоков

--- pages/card-container.php
+++ pages/card-container.php
@@ -2,7 +2,9 @@
 
     <div class="card">
         <div class="infimation">
-            <p>Тут будет много текста</p>
+            <h5>Бард</h5>
+            <p class="en_title encaption_text text_gray_color">Bard</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
         </div>
         <div class="icon bard">
         </div>
@@ -10,7 +12,9 @@
  
     <div class="card">
         <div class="infimation">
-            <p>Тут будет много текста</p>
+            <h5>Варвар</h5>
+            <p class="en_title encaption_text text_gray_color">Barbarian</p>
+            <p class="additionally uppercase_text">PBH / HIT D12</p>
         </div>
         <div class="icon barbarian">
         </div>
@@ -18,7 +22,9 @@
  
     <div class="card">
         <div class="infimation">
-            <p>Тут будет много текста</p>
+            <h5>Воин</h5>
+            <p class="en_title encaption_text text_gray_color">Fighter</p>
+            <p class="additionally uppercase_text">PBH / HIT D10</p>
         </div>
         <div class="icon fighter">
         </div>
@@ -26,9 +32,101 @@
 
     <div class="card">
         <div class="infimation">
-            <p>Тут будет много текста</p>
+            <h5>Волшебник</h5>
+            <p class="en_title encaption_text text_gray_color">Wizard</p>
+            <p class="additionally uppercase_text">PBH / HIT D6</p>
         </div>
         <div class="icon wizard">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Друид</h5>
+            <p class="en_title encaption_text text_gray_color">Druid</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
+        </div>
+        <div class="icon druid">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Жрец</h5>
+            <p class="en_title encaption_text text_gray_color">Cleric</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
+        </div>
+        <div class="icon cleric">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Изобретатель</h5>
+            <p class="en_title encaption_text text_gray_color">Artificer</p>
+            <p class="additionally uppercase_text">TCE / HIT D8</p>
+        </div>
+        <div class="icon artificer">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Колдун</h5>
+            <p class="en_title encaption_text text_gray_color">Warlock</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
+        </div>
+        <div class="icon warlock">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Монах</h5>
+            <p class="en_title encaption_text text_gray_color">Monk</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
+        </div>
+        <div class="icon monk">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Паладин</h5>
+            <p class="en_title encaption_text text_gray_color">Paladin</p>
+            <p class="additionally uppercase_text">PBH / HIT D10</p>
+        </div>
+        <div class="icon paladin">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Плут</h5>
+            <p class="en_title encaption_text text_gray_color">Rogue</p>
+            <p class="additionally uppercase_text">PBH / HIT D8</p>
+        </div>
+        <div class="icon rogue">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Следопыт</h5>
+            <p class="en_title encaption_text text_gray_color">Ranger</p>
+            <p class="additionally uppercase_text">PBH / HIT D10</p>
+        </div>
+        <div class="icon ranger">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="infimation">
+            <h5>Чародей</h5>
+            <p class="en_title encaption_text text_gray_color">Sorcerer</p>
+            <p class="additionally uppercase_text">PBH / HIT D6</p>
+        </div>
+        <div class="icon sorcerer">
         </div>
     </div>
     
