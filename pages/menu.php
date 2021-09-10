eda63d8347698a9f90c9cbfdaa40d67173d29b4f
Peterko <peterkojob@gmail.com>
2021-09-10 15:55:05 +0300

    Update
    + menu
    + dark  mod

---
+++ pages/menu.php
@@ -0,0 +1,20 @@
+<nav id="navbar" class="navbar">
+    <a class="logo icon_logo_menu" href="index.php"></a>
+    <ul class="navbar_nav">
+        <li class="nav_item">
+            <a class="nav_link" href="classes.php"><div class="nav_icon icon_character"></div><span>Персонаж</span></a>
+        </li>
+        <li class="nav_item">
+            <a class="nav_link" href="#"><div class="nav_icon icon_spells"></div><span>Заклинания</span></a>
+        </li>
+        <li class="nav_item">
+            <a class="nav_link" href="#"><div class="nav_icon icon_inventory"></div><span>Инвентарь</span></a>
+        </li>
+        <li onclick="switchTheme()" class="nav_item">
+            <a class="nav_link" href="#"><div class="nav_icon icon_dark_mod"></div><span>Dark Mod</span></a>
+        </li>
+    </ul>
+    <div class="copyright_block">
+        <p class="caption_text">Материалы на данном сайте приведены исключительно в справочных и ознакомительных целях и не заменяют необходимость покупки официальных материалов по игре. Весь графический материал и система Dungeons&Dragons является собственностью корпорации Wizards of the Coast.</p>
+    </div>
+</nav>
