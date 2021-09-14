800d6021df9438ed46452ba700780f1f93d3a3ce
Peterko <peterkojob@gmail.com>
2021-09-14 17:19:02 +0300

    Обновление меню.

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,7 +1,7 @@
 let arrow = document.querySelectorAll(".arrow");
 	for (var i = 0; i < arrow.length; i++) {
 		arrow[i].addEventListener("click", (e)=>{
-		let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+		let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
 		arrowParent.classList.toggle("showMenu");
 		});
 	}
