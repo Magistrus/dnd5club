2c28c984d126eb4f4bff11a5663bb88570423433
Виталий <@172.19.251.116>
2021-09-21 12:32:26 +0300

    добавил состояния (выбор, отображение)

---
+++ src/main/java/club/dnd5/portal/controller/ConditionController.java
@@ -0,0 +1,34 @@
+package club.dnd5.portal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import club.dnd5.portal.repository.ConditionRepository;
+
+@Controller
+public class ConditionController {
+	@Autowired
+	private ConditionRepository conditionRepository;
+	
+	@GetMapping("/conditions")
+	public String getConditions(Model model) {
+		model.addAttribute("conditions", conditionRepository.findAll());
+		return "conditions";
+	}
+
+	@GetMapping("/conditions//{name}")
+	public String getCondition(Model model, @PathVariable String name) {
+		model.addAttribute("conditions", conditionRepository.findAll());
+		model.addAttribute("selectedCondition", name);
+		return "conditions";
+	}
+
+	@GetMapping("/conditions/fragment/{name}")
+	public String getFragmentCondition(Model model, @PathVariable String name) {
+		model.addAttribute("condition", conditionRepository.findOneByEnglishName(name.replace("_", " ")));
+		return "fragments/condition :: view";
+	}
+}
