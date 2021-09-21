b5ca925c09ee3e2f93bac0d549a36ab0e8928982
Виталий <@172.19.251.116>
2021-09-21 18:04:47 +0300

    скролируемый тоолтип

--- src/main/resources/static/js/classes.js
+++ src/main/resources/static/js/classes.js
@@ -10,13 +10,15 @@ $(document).ready(function() {
 	        	};
 	        }
 	      },
-	      afterUpdate: function(content, element) {
-	    	  content.classList.add('tooltip_scroll');
-	    	  content.setAttribute('data-simplebar','');
-	      },
-          onShow: function(content, element) {
-          },
-		  maxWidth: 350,
+		  afterUpdate: function(content, element) {
+		   	simpleBar = new SimpleBar(content);
+		   	content.classList.add('tooltip_scroll');
+		  },
+		  onShow: function(content, element) {
+		   	simpleBar = new SimpleBar(content);
+		   	simpleBar.recalculate();
+		  },
+	      maxWidth: 350,
 	      skin: localStorage.getItem('theme'),
 	      radius: false,
 	      close: true,
