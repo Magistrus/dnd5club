efcbdf17f51463b489396ef636d5ade141d7e440
Виталий <@DESKTOP-BIPJIT7>
2021-10-04 10:02:38 +0300

    update background

--- src/main/java/club/dnd5/portal/controller/BackgroundController.java
+++ src/main/java/club/dnd5/portal/controller/BackgroundController.java
@@ -1,5 +1,10 @@
 package club.dnd5.portal.controller;
 
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.naming.directory.InvalidAttributesException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import club.dnd5.portal.model.background.Background;
+import club.dnd5.portal.model.background.Personalization;
+import club.dnd5.portal.model.background.PersonalizationType;
 import club.dnd5.portal.repository.datatable.BackgroundDatatableRepository;
 
 @Controller
@@ -28,7 +36,11 @@ public class BackgroundController {
 	
 	@GetMapping("/backgrounds/fragment/{id}")
 	public String getBackgroundFragmentById(Model model, @PathVariable Integer id) throws InvalidAttributesException {
-		model.addAttribute("background", repository.findById(id).orElseThrow(InvalidAttributesException::new));
+		Background background = repository.findById(id).orElseThrow(InvalidAttributesException::new);
+		model.addAttribute("background", background);
+		Map<PersonalizationType, List<Personalization>> tables = background.getPersonalizations().stream()
+				.collect(Collectors.groupingBy(Personalization::getType, () -> new EnumMap<>(PersonalizationType.class), Collectors.toList()));
+		model.addAttribute("tables", tables);
 		return "fragments/background :: view";
 	}
 }
