899cfd4c5232d11f9168d00fcca5f00069fb2516
Magistrus <gm.magistr@gmail.com>
2021-11-08 17:36:02 +0200

    fix open weapon in mobile

--- src/main/resources/static/js/datatables/weapons.js
+++ src/main/resources/static/js/datatables/weapons.js
@@ -74,6 +74,9 @@ $(document).ready(function() {
 	});
 
 	$('#weapons tbody').on('click', 'tr', function () {
+		if(!document.getElementById('list_page_two_block').classList.contains('block_information')){
+			document.getElementById('list_page_two_block').classList.add('block_information');
+		}
 		var tr = $(this).closest('tr');
 		var table = $('#weapons').DataTable();
 		var row = table.row( tr );
