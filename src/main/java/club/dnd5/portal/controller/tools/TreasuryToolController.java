11bd03a9436176a498a8594ce6ea21bc0cf8efeb
Magistrus <gm.magistr@gmail.com>
2021-11-22 15:39:31 +0200

    fix menu

--- src/main/java/club/dnd5/portal/controller/tools/TreasuryToolController.java
+++ src/main/java/club/dnd5/portal/controller/tools/TreasuryToolController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TreasuryToolController {
-	@GetMapping("/treasury")
+	@GetMapping("/tools/treasury")
 	public String getTreasuryTool() {
 		return "tools/treasury";
 	}
