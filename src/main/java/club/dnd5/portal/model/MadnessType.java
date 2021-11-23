473c3dc4535d9372550aab79692ddd7458c6407a
Magistrus <gm.magistr@gmail.com>
2021-11-23 13:56:14 +0200

    update madness tool

---
+++ src/main/java/club/dnd5/portal/model/MadnessType.java
@@ -0,0 +1,15 @@
+package club.dnd5.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum MadnessType {
+	SHORT("кратковременное","1к10 минут"),
+	LONG("долговременное","1к10 × 10 часов"), 
+	UNLIMITED("бессрочное", "до исцеления");
+	
+	private String cyrilicName;
+	private String duration;
+}
