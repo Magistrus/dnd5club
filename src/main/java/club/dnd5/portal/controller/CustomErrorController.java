d2af9e7ffc135b3671d45adfd7aeb26a77575566
Magistrus <gm.magistr@gmail.com>
2021-11-23 14:39:35 +0200

    fix error page

--- src/main/java/club/dnd5/portal/controller/CustomErrorController.java
+++ src/main/java/club/dnd5/portal/controller/CustomErrorController.java
@@ -1,5 +1,8 @@
 package club.dnd5.portal.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
     @GetMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            switch (statusCode) {
+                case 404:
+                    return "error_404";
+
+                case 500:
+                    return "error_500";
+            }
+        }
         return "error";
     }
 }
