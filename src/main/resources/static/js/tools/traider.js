3d8365c91cba7b30b91c5534129389fb682165a2
Magistrus <gm.magistr@gmail.com>
2021-11-26 22:52:38 +0200

    fix trader

--- src/main/resources/static/js/tools/traider.js
+++ src/main/resources/static/js/tools/traider.js
@@ -1,6 +1,6 @@
 $('#generate').on('click', function() {
-	var url = '/tools/trader/result/?result=25&mageRang=1';
-	$("#left_content_block").load(url);
+	const url = '/tools/trader/result/?result='+ $('#result').val() +'&mageRang=' + $('#mageRang').val();
+	$("#trader_content").load(url);
 });
 $('#clear').on('click', function() {
 	  const myNode = document.getElementById("content_block");
