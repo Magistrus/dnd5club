1477cae564a9939c70a5ca860c42489aaf17cf80
Peterko <peterkojob@gmail.com>
2021-12-18 00:24:59 +0300

    Fix

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -138,19 +138,3 @@ $('#setting_source').change(function() {
 $('li').click(function () {
 	localStorage.setItem('selected_item_menu', this.id);
 });
-
-function getScrollParent(node) {
-	const isElement = node instanceof HTMLElement;
-	const overflowY = isElement && window.getComputedStyle(node).overflowY;
-	const isScrollable = overflowY !== 'visible' && overflowY !== 'hidden';
-  
-	if (!node) {
-	  return null;
-	} else if (isScrollable && node.scrollHeight >= node.clientHeight) {
-	  return node;
-	}
-  
-	return getScrollParent(node.parentNode) || document.body;
-  }
-  
-  export default getScrollParent;
