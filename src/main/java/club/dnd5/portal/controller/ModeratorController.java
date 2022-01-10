3cb4fdb925c53b1c4a657ef32378a2c858ee996e
Magistrus <gm.magistr@gmail.com>
2022-01-10 21:52:26 +0200

    fix add monster

--- src/main/java/club/dnd5/portal/controller/ModeratorController.java
+++ src/main/java/club/dnd5/portal/controller/ModeratorController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import club.dnd5.portal.model.Alignment;
 import club.dnd5.portal.model.CreatureSize;
 import club.dnd5.portal.model.CreatureType;
+import club.dnd5.portal.model.DamageType;
+import club.dnd5.portal.model.creature.Condition;
 
 @Controller
 public class ModeratorController {
@@ -15,6 +17,10 @@ public class ModeratorController {
 		model.addAttribute("sizes", CreatureSize.values());
 		model.addAttribute("types", CreatureType.values());
 		model.addAttribute("aligments", Alignment.values());
+		model.addAttribute("resistancs", DamageType.getResistance());
+		model.addAttribute("resistancs", DamageType.getImmunity());
+		model.addAttribute("vulnerabilities", DamageType.getVulnerability());
+		model.addAttribute("conditionImmunity", Condition.values());
 		return "user/admin/add_beast";
 	}
 }
