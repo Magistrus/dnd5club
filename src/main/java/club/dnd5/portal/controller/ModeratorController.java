9715440a08aa7e2160cbde0d85a66a74139ff9e7
Magistrus <gm.magistr@gmail.com>
2022-01-10 21:38:42 +0200

    add monster

--- src/main/java/club/dnd5/portal/controller/ModeratorController.java
+++ src/main/java/club/dnd5/portal/controller/ModeratorController.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import club.dnd5.portal.model.Alignment;
+import club.dnd5.portal.model.CreatureSize;
+import club.dnd5.portal.model.CreatureType;
+
 @Controller
 public class ModeratorController {
 	@GetMapping ("/profile/beast")
 	public String getProfileForm(Model model) {
+		model.addAttribute("sizes", CreatureSize.values());
+		model.addAttribute("types", CreatureType.values());
+		model.addAttribute("aligments", Alignment.values());
 		return "user/admin/add_beast";
 	}
 }
