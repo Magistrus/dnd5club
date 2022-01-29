4f1ad3b42cfaf026cc0d0c5d3a69fc712aed5979
Magistrus <gm.magistr@gmail.com>
2022-01-29 22:14:00 +0200

    experement 4

--- src/main/java/club/dnd5/portal/model/foundary/FBeastiary.java
+++ src/main/java/club/dnd5/portal/model/foundary/FBeastiary.java
@@ -2,6 +2,11 @@ package club.dnd5.portal.model.foundary;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class FBeastiary {
 	List<FCreature> monster;
 	public FBeastiary(List<FCreature> creatures) {
