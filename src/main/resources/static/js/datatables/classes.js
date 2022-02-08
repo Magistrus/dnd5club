665d3338679408c64e5c03a83c793028afc9f8b1
Peterko <peterkojob@gmail.com>
2022-02-08 12:57:55 +0300

    Update

--- src/main/resources/static/js/datatables/classes.js
+++ src/main/resources/static/js/datatables/classes.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
 			data : "name",
 			render : function(data, type, row) {
 				if (type === 'display') {
-					var result ='<div class="content"><h3 class="row_name"><span>' + row.name;
-					result+='</span><span>[' + row.englishName + ']</span></h3>';
+					var result ='<div class="info_block">1</div><div class="content"><h3 class="row_name"><span>' + row.name;
+					result+='</span><span>[' + row.englishName + ']</span></h3></div>';
 					return result;
 				}
 				return data;
