14a5f592c447c8c7503f1c332ef07f3a56c02628
Magistrus <gm.magistr@gmail.com>
2022-03-07 18:34:40 +0200

    fix race view

--- src/main/java/club/dnd5/portal/dto/RaceDto.java
+++ src/main/java/club/dnd5/portal/dto/RaceDto.java
@@ -40,9 +40,9 @@ public class RaceDto {
 		icon = race.getIcon() == null ? "" : race.getIcon();
 		
 		hasSubraces = !race.getSubRaces().isEmpty();
-		subraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.OFFICAL).map(RaceDto::new).collect(Collectors.toList());
-		settingSubraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.SETTING).map(RaceDto::new).collect(Collectors.toList());
-		homebrewSubraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.CUSTOM).map(RaceDto::new).collect(Collectors.toList());
+		subraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.OFFICAL && !a.isView()).map(RaceDto::new).collect(Collectors.toList());
+		settingSubraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.SETTING && !a.isView()).map(RaceDto::new).collect(Collectors.toList());
+		homebrewSubraces = race.getSubRaces().stream().filter(a-> a.getBook().getType() == TypeBook.CUSTOM && !a.isView()).map(RaceDto::new).collect(Collectors.toList());
 		homebrew = race.getBook().getType() == TypeBook.CUSTOM;
 		setting = race.getBook().getType() == TypeBook.SETTING;
 		book = race.getBook().getName();
