be409c4cb60a342d56f7c5964590f2ef5fac7933
svifty7 <svifty7@gmail.com>
2022-03-21 22:49:59 +0300

    added timestamp in include scripts/styles, added textarea-editor

---
+++ src/main/resources/static/js/articlesForm.js
@@ -0,0 +1,8 @@
+$('#text').trumbowyg({
+    lang: 'ru',
+    tagsToRemove: [ 'script', 'link' ],
+    autogrowOnEnter: true,
+    urlProtocol: true,
+    defaultLinkTarget: '_blank',
+    svgPath: '/resources/assets/trumbowyg-icons.svg',
+})
