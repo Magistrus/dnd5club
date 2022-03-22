ca5d3d27c5df9fce10921d02ed72eb65b1b846a0
Magistrus <gm.magistr@gmail.com>
2022-03-22 13:57:32 +0200

    fix profiles

--- src/main/java/club/dnd5/portal/controller/ProfileController.java
+++ src/main/java/club/dnd5/portal/controller/ProfileController.java
@@ -1,11 +1,17 @@
 package club.dnd5.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import club.dnd5.portal.service.ArticleService;
+
 @Controller
-public class UserProfileController {
+public class ProfileController {
+	@Autowired
+	private ArticleService service;
+	
 	@GetMapping ("/profile")
 	public String getProfileForm(Model model) {
 		return "user/profile";
--- src/main/java/club/dnd5/portal/service/ArticleService.java
+++ src/main/java/club/dnd5/portal/service/ArticleService.java
@@ -14,4 +14,5 @@ public interface ArticleService {
 	Collection<Article> findAllByCreator(User user);
 
 	Article save(Article article, User creator);
+	
 }
