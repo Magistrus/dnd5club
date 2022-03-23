810e1e4b337c8334bedadfb23fb6a677e5731d65
Magistrus <gm.magistr@gmail.com>
2022-03-23 13:34:58 +0200

    fix users

--- src/main/java/club/dnd5/portal/controller/AdminController.java
+++ src/main/java/club/dnd5/portal/controller/AdminController.java
@@ -1,6 +1,7 @@
 package club.dnd5.portal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,13 @@ public class AdminController {
 	@Autowired
 	private RoleRepository roleRepository;
 	
+	@Value("${git.commit.id}")
+	private String version;
+	
 	@GetMapping("/admin/users")
 	public String getUsers(Model model) {
 		model.addAttribute("roles", roleRepository.findAll());
+		model.addAttribute("version", version);
 		return "/user/admin/users";
 	}
 }
