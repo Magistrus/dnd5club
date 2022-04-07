bb0f6d64adc89dc36fca43dff024b59e4f7986f6
svifty7 <svifty7@gmail.com>
2022-04-08 02:44:53 +0300

    add js post query

--- src/main/resources/static/js/articlesForm.js
+++ src/main/resources/static/js/articlesForm.js
@@ -1,5 +1,30 @@
 const realTextarea = document.getElementById('text');
 const textContent = realTextarea.value;
+const form = document.getElementById('article_edit');
+
+let savingDebounce;
+
+function saveForm() {
+    if (savingDebounce) {
+        clearTimeout(savingDebounce);
+    }
+
+    savingDebounce = setTimeout(function () {
+        const body = new FormData(form);
+
+        body.append('save', 'save');
+
+        fetch(form.getAttribute('action'), {
+            method: form.getAttribute('method'),
+            redirect: 'follow',
+            body
+        }).catch(function (err) {
+            if (err.name !== 'AbortError') {
+                throw new Error(err);
+            }
+        });
+    }, 1200);
+}
 
 let parsedContent = {};
 
@@ -24,10 +49,11 @@ const editorJSOptions = {
         },
         header: {
             class: Header,
+            inlineToolbar: true,
             config: {
                 placeholder: 'Заголовок',
                 levels: [3, 4, 5],
-                defaultLevel: 3
+                defaultLevel: 4
             }
         },
         inlineCode: {
@@ -140,13 +166,19 @@ const editorJSOptions = {
     },
     onChange: function () {
         editor.save()
-            .then(function (output) {
+            .then(async function (output) {
                 realTextarea.value = JSON.stringify(output);
+
+                saveForm();
             })
             .catch(function (err) {
                 console.error(err);
             })
     }
-}
+};
 
 const editor = new EditorJS(editorJSOptions);
+
+for (let i = 0; i < form.elements.length; i++) {
+    form.elements[i].addEventListener('input', saveForm);
+}
