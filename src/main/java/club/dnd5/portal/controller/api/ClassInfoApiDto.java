767840a4f99b2f9147f607259d04d6dd15bc39c8
Magistrus <gm.magistr@gmail.com>
2022-04-27 15:17:44 +0300

    fix class info api

--- src/main/java/club/dnd5/portal/controller/api/ClassInfoApiDto.java
+++ src/main/java/club/dnd5/portal/controller/api/ClassInfoApiDto.java
@@ -1,6 +1,10 @@
 package club.dnd5.portal.controller.api;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import club.dnd5.portal.dto.api.classes.NameApiDto;
 import club.dnd5.portal.model.SpellcasterType;
@@ -8,17 +12,25 @@ import club.dnd5.portal.model.classes.HeroClass;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonInclude(Include.NON_NULL)
+
 @Getter
 @Setter
 public class ClassInfoApiDto {
 	private NameApiDto name;
-	private boolean spellcaster;
-	private String optionName;
+	private Collection<ClassTabApiDto> tabs = new ArrayList<>(5);
 	private Collection<String> images;
+
 	public ClassInfoApiDto(HeroClass heroClass, Collection<String> images) {
 		name = new NameApiDto(heroClass.getCapitalazeName(), heroClass.getEnglishName());
-		spellcaster = heroClass.getSpellcasterType() != SpellcasterType.NONE;
-		optionName = heroClass.getOptionType().getDisplayName();
 		this.images = images;
+		tabs.add(new ClassTabApiDto("Навыки", String.format("/classes/%s/traits", heroClass.getUrlName()), "tab-traits", 0));
+		tabs.add(new ClassTabApiDto("Описание", String.format("/classes/%s/description", heroClass.getUrlName()), "tab_description", 1));
+		if (heroClass.getSpellcasterType() != SpellcasterType.NONE) {
+			tabs.add(new ClassTabApiDto("Заклинания", String.format("/classes/%s/description", heroClass.getUrlName()), "tab-spells", 2));	
+		}
+		if (heroClass.getOptionType() != null) {
+			tabs.add(new ClassTabApiDto(heroClass.getOptionType().getDisplayName(), String.format("/classes/%s/options", heroClass.getUrlName()), "tab-option", 3));
+		}
 	}
 }
---
+++ src/main/java/club/dnd5/portal/controller/api/ClassTabApiDto.java
@@ -0,0 +1,14 @@
+package club.dnd5.portal.controller.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+
+@Getter
+public class ClassTabApiDto {
+	private String name;
+	private String url;
+	private String icon;
+	private int order;
+}
