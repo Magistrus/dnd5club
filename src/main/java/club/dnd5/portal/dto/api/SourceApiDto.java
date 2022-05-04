26012c32d8b3a3bda860cd44fe339a263b6b185b
Magistrus <gm.magistr@gmail.com>
2022-05-04 17:17:39 +0300

    fix class api

--- src/main/java/club/dnd5/portal/dto/api/SourceApiDto.java
+++ src/main/java/club/dnd5/portal/dto/api/SourceApiDto.java
@@ -1,9 +1,14 @@
 package club.dnd5.portal.dto.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import club.dnd5.portal.model.book.Book;
 import club.dnd5.portal.model.book.TypeBook;
 import lombok.Getter;
 import lombok.Setter;
+
+@JsonInclude(Include.NON_NULL)
 
 @Getter
 @Setter
