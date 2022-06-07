233bc5d7544128785e5e2394265ab93c42d1feca
Magistrus <gm.magistr@gmail.com>
2022-06-07 13:01:03 +0300

    add armors api

---
+++ src/main/java/club/dnd5/portal/controller/api/item/ArmorApiController.java
@@ -0,0 +1,110 @@
+package club.dnd5.portal.controller.api.item;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.datatables.mapping.Column;
+import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
+import org.springframework.data.jpa.datatables.mapping.Search;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import club.dnd5.portal.dto.api.item.ArmorApi;
+import club.dnd5.portal.dto.api.item.ArmorDetailApi;
+import club.dnd5.portal.dto.api.item.ArmorRequesApi;
+import club.dnd5.portal.model.book.Book;
+import club.dnd5.portal.model.items.Armor;
+import club.dnd5.portal.model.splells.Spell;
+import club.dnd5.portal.repository.datatable.ArmorDatatableRepository;
+
+@RestController
+public class ArmorApiController {
+	@Autowired
+	private ArmorDatatableRepository repo;
+	
+	@PostMapping(value = "/api/v1/armors", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<ArmorApi> getItem(@RequestBody ArmorRequesApi request) {
+		Specification<Armor> specification = null;
+
+		DataTablesInput input = new DataTablesInput();
+		List<Column> columns = new ArrayList<Column>(3);
+		Column column = new Column();
+		column.setData("name");
+		column.setName("name");
+		column.setSearchable(Boolean.TRUE);
+		column.setOrderable(Boolean.TRUE);
+		column.setSearch(new Search("", Boolean.FALSE));
+		columns.add(column);
+		
+		column = new Column();
+		column.setData("englishName");
+		column.setName("englishName");
+		column.setSearch(new Search("", Boolean.FALSE));
+		column.setSearchable(Boolean.TRUE);
+		column.setOrderable(Boolean.TRUE);
+		columns.add(column);
+		
+		column = new Column();
+		column.setData("altName");
+		column.setName("altName");
+		column.setSearchable(Boolean.TRUE);
+		column.setOrderable(Boolean.FALSE);
+		columns.add(column);
+		
+		input.setColumns(columns);
+		input.setLength(-1);
+		if (request.getSearch() != null) {
+			if (request.getSearch().getValue() != null && !request.getSearch().getValue().isEmpty()) {
+				if (request.getSearch().getExact() != null && request.getSearch().getExact()) {
+					specification = (root, query, cb) -> cb.equal(root.get("name"), request.getSearch().getValue().trim().toUpperCase());
+				} else {
+					input.getSearch().setValue(request.getSearch().getValue());
+					input.getSearch().setRegex(Boolean.FALSE);
+				}
+			}
+		}
+		if (request.getFilter() != null) {
+
+			if (!request.getFilter().getBooks().isEmpty()) {
+				specification = addSpecification(specification, (root, query, cb) -> {
+					Join<Book, Spell> join = root.join("book", JoinType.INNER);
+					return join.get("source").in(request.getFilter().getBooks());
+				});
+			}
+		}
+		if (request.getOrders()!=null && !request.getOrders().isEmpty()) {
+			specification = addSpecification(specification, (root, query, cb) -> {
+				List<Order> orders = request.getOrders().stream()
+						.map(
+							order -> "asc".equals(order.getDirection()) ? cb.asc(root.get(order.getField())) : cb.desc(root.get(order.getField()))
+						)
+						.collect(Collectors.toList());
+				query.orderBy(orders);
+				return cb.and();
+			});
+		}
+		return repo.findAll(input, specification, specification, ArmorApi::new).getData();
+	}
+	
+	@PostMapping(value = "/api/v1/armors/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ArmorDetailApi getOption(@PathVariable String englishName) {
+		return new ArmorDetailApi(repo.findByEnglishName(englishName.replace('_', ' ')));
+	}
+	
+	private <T> Specification<T> addSpecification(Specification<T> specification, Specification<T> addSpecification) {
+		if (specification == null) {
+			return Specification.where(addSpecification);
+		}
+		return specification.and(addSpecification);
+	}
+}
