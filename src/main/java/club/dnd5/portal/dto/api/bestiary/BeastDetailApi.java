082f989c43403d98153de54ec1dcc84a443e2bd0
Magistrus <gm.magistr@gmail.com>
2022-06-08 11:16:06 +0300

    update beast api

--- src/main/java/club/dnd5/portal/dto/api/bestiary/BeastDetailApi.java
+++ src/main/java/club/dnd5/portal/dto/api/bestiary/BeastDetailApi.java
@@ -3,16 +3,15 @@ package club.dnd5.portal.dto.api.bestiary;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.hibernate.hql.internal.ast.tree.MapValueNode;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import club.dnd5.portal.dto.api.NameValueApi;
 import club.dnd5.portal.model.AbilityType;
 import club.dnd5.portal.model.ArmorType;
 import club.dnd5.portal.model.DamageType;
+import club.dnd5.portal.model.creature.Condition;
 import club.dnd5.portal.model.creature.Creature;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -42,7 +41,10 @@ public class BeastDetailApi extends BeastApi {
 	
 	private List<NameValueApi> savingThrows;
 	private List<NameValueApi> skills;
+	
 	private List<String> damageResistances;
+	private List<String> damageImmunities;
+	private List<String> conditionImmunities;
 	
 	private String description;
 
@@ -71,10 +73,16 @@ public class BeastDetailApi extends BeastApi {
 			savingThrows = beast.getSavingThrows().stream().map(st -> new NameValueApi(st.getAbility().getCyrilicName(), st.getBonus())).collect(Collectors.toList());
 		}
 		if (!beast.getSkills().isEmpty()) {
-			skills = beast.getSkills().stream().map(skill -> new NameValueApi(skill.getCyrilicText(), skill.getBonus())).collect(Collectors.toList());
+			skills = beast.getSkills().stream().map(skill -> new NameValueApi(skill.getType().getCyrilicName(), skill.getBonus())).collect(Collectors.toList());
 		}
 		if (!beast.getResistanceDamages().isEmpty()) {
 			damageResistances = beast.getResistanceDamages().stream().map(DamageType::getCyrilicName).collect(Collectors.toList());
+		}
+		if (!beast.getImmunityDamages().isEmpty()) {
+			damageImmunities = beast.getImmunityDamages().stream().map(DamageType::getCyrilicName).collect(Collectors.toList());
+		}
+		if (!beast.getImmunityStates().isEmpty()) {
+			conditionImmunities = beast.getImmunityStates().stream().map(Condition::getCyrilicName).collect(Collectors.toList());
 		}
 		description = beast.getDescription();
 	}
