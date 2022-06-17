3aba1c5bdb4b13952d2b003a4d7ca62f76c3b709
Magistrus <gm.magistr@gmail.com>
2022-06-17 22:27:35 +0300

    fix treasure

--- src/main/java/club/dnd5/portal/model/Dice.java
+++ src/main/java/club/dnd5/portal/model/Dice.java
@@ -1,6 +1,8 @@
 package club.dnd5.portal.model;
 
 import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
@@ -9,34 +11,28 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Dice {
-	d4(4),
-	d6(6),
-	d8(8),
-	d10(10),
-	d12(12),
-	d20(20),
-	d100(100),
-	d3(3),
-	d2(2);
-	
+	d4(4), d6(6), d8(8), d10(10), d12(12), d20(20), d100(100), d3(3), d2(2);
+
+	private static Random rnd = new Random();
+
 	private int maxValue;
-	
+
 	public static Dice parse(int dice) {
-		switch(dice) {
-			case 4:
-				return d4;
-			case 6:
-				return d6;
-			case 8:
-				return d8;
-			case 10:
-				return d10;
-			case 12:
-				return d12;
-			case 20:
-				return d20;
-			case 100:
-				return d100;
+		switch (dice) {
+		case 4:
+			return d4;
+		case 6:
+			return d6;
+		case 8:
+			return d8;
+		case 10:
+			return d10;
+		case 12:
+			return d12;
+		case 20:
+			return d20;
+		case 100:
+			return d100;
 		}
 		return null;
 	}
@@ -48,9 +44,9 @@ public enum Dice {
 	public static Set<Dice> getCreatures() {
 		return EnumSet.of(d4, d6, d8, d10, d12, d20);
 	}
-	
+
 	public static Dice parse(String dice) {
-		switch(dice) {
+		switch (dice) {
 		case "k4":
 			return d4;
 		case "k6":
@@ -65,7 +61,20 @@ public enum Dice {
 			return d20;
 		case "k100":
 			return d100;
+		}
+		return null;
 	}
-	return null;
+
+	public static int roll(Dice dice) {
+		return 1 + rnd.nextInt(dice.maxValue);
 	}
+
+	public static int roll(int countDice, Dice dice) {
+		int result = 0;
+		for (int i = 0; i < countDice; i++) {
+			result += roll(dice);
+		}
+		return result;
+	}
+
 }
