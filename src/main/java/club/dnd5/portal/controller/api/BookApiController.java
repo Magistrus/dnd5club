ccd8524df3c8fe8a2a88708609903d88b41465ae
Magistrus <gm.magistr@gmail.com>
2022-06-18 16:57:17 +0300

    add books api

--- src/main/java/club/dnd5/portal/controller/api/BookApiController.java
+++ src/main/java/club/dnd5/portal/controller/api/BookApiController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Order;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,22 +17,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import club.dnd5.portal.dto.api.classes.OptionApi;
-import club.dnd5.portal.dto.api.classes.OptionDetailApi;
-import club.dnd5.portal.dto.api.classes.OptionRequesApi;
+import club.dnd5.portal.dto.api.BookApi;
+import club.dnd5.portal.dto.api.BookRequestApi;
 import club.dnd5.portal.model.book.Book;
-import club.dnd5.portal.model.classes.Option;
-import club.dnd5.portal.model.splells.Spell;
-import club.dnd5.portal.repository.datatable.OptionDatatableRepository;
+import club.dnd5.portal.repository.datatable.BookDatatableRepository;
+import club.dnd5.portal.util.SpecificationUtil;
 
 @RestController
-public class OptionApiController {
+public class BookApiController {
 	@Autowired
-	private OptionDatatableRepository repo;
-	
-	@PostMapping(value = "/api/v1/options", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<OptionApi> getOptions(@RequestBody OptionRequesApi request) {
-		Specification<Option> specification = null;
+	private BookDatatableRepository repo;
+
+	@PostMapping(value = "/api/v1/books", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<BookApi> getBooks(@RequestBody BookRequestApi request) {
+		Specification<Book> specification = null;
 
 		DataTablesInput input = new DataTablesInput();
 		List<Column> columns = new ArrayList<Column>(3);
@@ -45,7 +41,7 @@ public class OptionApiController {
 		column.setOrderable(Boolean.TRUE);
 		column.setSearch(new Search("", Boolean.FALSE));
 		columns.add(column);
-		
+
 		column = new Column();
 		column.setData("englishName");
 		column.setName("englishName");
@@ -53,62 +49,49 @@ public class OptionApiController {
 		column.setSearchable(Boolean.TRUE);
 		column.setOrderable(Boolean.TRUE);
 		columns.add(column);
-		
+
 		column = new Column();
 		column.setData("altName");
 		column.setName("altName");
 		column.setSearchable(Boolean.TRUE);
 		column.setOrderable(Boolean.FALSE);
 		columns.add(column);
-		
+
 		input.setColumns(columns);
 		input.setLength(request.getLimit() != null ? request.getLimit() : -1);
-		if (request.getPage() != null && request.getLimit()!=null) {
-			input.setStart(request.getPage() * request.getLimit());	
+		if (request.getPage() != null && request.getLimit() != null) {
+			input.setStart(request.getPage() * request.getLimit());
 		}
 		if (request.getSearch() != null) {
 			if (request.getSearch().getValue() != null && !request.getSearch().getValue().isEmpty()) {
 				if (request.getSearch().getExact() != null && request.getSearch().getExact()) {
-					specification = (root, query, cb) -> cb.equal(root.get("name"), request.getSearch().getValue().trim().toUpperCase());
+					specification = (root, query, cb) -> cb.equal(root.get("name"),
+							request.getSearch().getValue().trim().toUpperCase());
 				} else {
 					input.getSearch().setValue(request.getSearch().getValue());
 					input.getSearch().setRegex(Boolean.FALSE);
 				}
 			}
 		}
-		if (request.getFilter() != null) {
+		if (request.getOrders() != null && !request.getOrders().isEmpty()) {
 
-			if (!request.getFilter().getBooks().isEmpty()) {
-				specification = addSpecification(specification, (root, query, cb) -> {
-					Join<Book, Spell> join = root.join("book", JoinType.INNER);
-					return join.get("source").in(request.getFilter().getBooks());
-				});
-			}
-		}
-		if (request.getOrders()!=null && !request.getOrders().isEmpty()) {
-			
-			specification = addSpecification(specification, (root, query, cb) -> {
+			specification = SpecificationUtil.getAndSpecification(specification, (root, query, cb) -> {
 				List<Order> orders = request.getOrders().stream()
-						.map(
-							order -> "asc".equals(order.getDirection()) ? cb.asc(root.get(order.getField())) : cb.desc(root.get(order.getField()))
-						)
+						.map(order -> "asc".equals(order.getDirection()) ? cb.asc(root.get(order.getField()))
+								: cb.desc(root.get(order.getField())))
 						.collect(Collectors.toList());
 				query.orderBy(orders);
 				return cb.and();
 			});
 		}
-		return repo.findAll(input, specification, specification, OptionApi::new).getData();
+		return repo.findAll(input, specification, specification, BookApi::new).getData();
 	}
-	
-	@PostMapping(value = "/api/v1/options/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public OptionDetailApi getOption(@PathVariable String englishName) {
-		return new OptionDetailApi(repo.findByEnglishName(englishName.replace('_', ' ')));
-	}
-	
-	private <T> Specification<T> addSpecification(Specification<T> specification, Specification<T> addSpecification) {
-		if (specification == null) {
-			return Specification.where(addSpecification);
-		}
-		return specification.and(addSpecification);
+
+	@PostMapping(value = "/api/v1/books/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookApi getBook(@PathVariable String englishName) {
+		Book book = repo.findByEnglishName(englishName.replace('_', ' '));
+		BookApi bookApi = new BookApi(book);
+		bookApi.setDescription(book.getDescription());
+		return bookApi;
 	}
 }
