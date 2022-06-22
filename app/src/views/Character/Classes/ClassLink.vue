b5cc84a1c06fffdcba333cf1b38a023a8b12be4c
Peterko <peterko@gmail.com>
2022-06-22 14:05:25 +0300

    Update

--- app/src/views/Character/Classes/ClassLink.vue
+++ app/src/views/Character/Classes/ClassLink.vue
@@ -13,46 +13,52 @@
             v-bind="$attrs"
         >
             <div class="link-item-expand__content">
+                <img :src="`/assets/img/classes/${iconPath}.webp`" alt="img-bg" class="">
+
                 <div class="link-item-expand__main">
                     <a
                         :href="href"
                         class="link-item-expand__link"
-                        @click.left.prevent.exact="navigate()"
+                        @click.left.prevent.exact="selectClass(navigate)"
                     >
-                        <span class="link-item-expand__icon">
-                            <svg-icon
-                                :icon-name="classItem.icon"
-                                :stroke-enable="false"
-                                fill-enable
-                            />
-                        </span>
+                    
+                    <span class="link-item-expand__body">
+                        <span class="link-item-expand__body_row">
 
-                        <span class="link-item-expand__body">
-                            <span class="link-item-expand__body_row">
-                                <span class="link-item-expand__name">
-                                    <span class="link-item-expand__name--rus">
-                                        {{ classItem.name.rus }}
-                                    </span>
-
-                                    <span class="link-item-expand__name--eng">
-                                        [{{ classItem.name.eng }}]
-                                    </span>
-                                </span>
-
-                                <span
-                                    v-tooltip="{ content: classItem.source.name }"
-                                    class="link-item-expand__book"
-                                >
-                                    {{ classItem.source.shortName }}
-                                </span>
+                            <span class="link-item-expand__icon">
+                                <svg-icon
+                                    :icon-name="classItem.icon"
+                                    :stroke-enable="false"
+                                    fill-enable
+                                />
                             </span>
 
-                            <span class="link-item-expand__body_row">
-                                <span class="link-item-expand__dice">
-                                    {{ classItem.dice }}
+                            <span class="link-item-expand__name">
+                                <span class="link-item-expand__name--rus">
+                                    {{ classItem.name.rus }}
                                 </span>
+
+                                <span class="link-item-expand__name--eng">
+                                    {{ classItem.name.eng }}
+                                </span>
+
                             </span>
                         </span>
+
+                        <span class="link-item-expand__body_row">
+                            <span class="link-item-expand__dice">
+                                {{ classItem.dice }}
+                            </span>
+
+                            <span
+                                v-tooltip="{ content: classItem.source.name }"
+                                class="link-item-expand__book"
+                            >
+                                {{ classItem.source.shortName }}
+                            </span>
+                        </span>
+                    </span>
+                    
                     </a>
 
                     <button
@@ -69,7 +75,7 @@
 
                 <div
                     v-if="hasArchetypes"
-                    :class="{ 'is-active': isOpenedArchetypes }"
+                    :class="{ 'is-active': submenu.show }"
                     class="link-item-expand__arch-list"
                 >
                     <div
@@ -139,16 +145,12 @@
             }
         },
         computed: {
-            isOpenedArchetypes() {
-                if (this.$route.params?.className === this.classItem.url) {
-                    return true
-                }
-
-                return this.submenu.show
-            },
-
             hasArchetypes() {
                 return !!this.classItem?.archetypes?.length
+            },
+
+            iconPath() {
+                return this.classItem?.name?.eng?.trim()?.toLowerCase()?.replaseAll(' ', '')
             }
         },
         watch: {
@@ -177,6 +179,12 @@
 
             updateGrid() {
                 this.$nextTick(() => this.$redrawVueMasonry('class-links'))
+            },
+
+            selectClass(callback) {
+                this.submenu.show = true;
+
+                callback();
             },
         }
     }
