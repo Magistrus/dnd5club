ff8f9488e8574beb376b2840eec4afdc8ee72358
Magistrus <gm.magistr@gmail.com>
2022-06-24 15:36:36 +0300

    add beast filter api

--- src/main/java/club/dnd5/portal/controller/api/bestiary/BeastApiConroller.java
+++ src/main/java/club/dnd5/portal/controller/api/bestiary/BeastApiConroller.java
@@ -1,15 +1,20 @@
 package club.dnd5.portal.controller.api.bestiary;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.datatables.mapping.Column;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.Search;
@@ -22,26 +27,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import club.dnd5.portal.dto.api.FilterApi;
+import club.dnd5.portal.dto.api.FilterValueApi;
 import club.dnd5.portal.dto.api.bestiary.BeastApi;
 import club.dnd5.portal.dto.api.bestiary.BeastDetailApi;
 import club.dnd5.portal.dto.api.bestiary.BeastlRequesApi;
+import club.dnd5.portal.model.CreatureSize;
+import club.dnd5.portal.model.CreatureType;
 import club.dnd5.portal.model.book.Book;
+import club.dnd5.portal.model.book.TypeBook;
 import club.dnd5.portal.model.creature.Creature;
+import club.dnd5.portal.model.creature.HabitatType;
 import club.dnd5.portal.model.foundary.FBeastiary;
 import club.dnd5.portal.model.fvtt.plutonium.FBeast;
 import club.dnd5.portal.model.image.ImageType;
+import club.dnd5.portal.model.splells.MagicSchool;
 import club.dnd5.portal.model.splells.Spell;
 import club.dnd5.portal.repository.ImageRepository;
 import club.dnd5.portal.repository.datatable.BestiaryDatatableRepository;
+import club.dnd5.portal.repository.datatable.TagBestiaryDatatableRepository;
 import club.dnd5.portal.util.SpecificationUtil;
 
 @RestController
 public class BeastApiConroller {
 	@Autowired
-	private BestiaryDatatableRepository repo;
-
+	private BestiaryDatatableRepository beastRepository;
+	
 	@Autowired
-	private ImageRepository imageRepo;
+	private TagBestiaryDatatableRepository tagRepository;
+	
+	@Autowired
+	private ImageRepository imageRepository;
 
 	@PostMapping(value = "/api/v1/bestiary", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<BeastApi> getBestiary(@RequestBody BeastlRequesApi request) {
@@ -110,14 +126,14 @@ public class BeastApiConroller {
 				});
 			}
 		}
-		return repo.findAll(input, specification, specification, BeastApi::new).getData();
+		return beastRepository.findAll(input, specification, specification, BeastApi::new).getData();
 	}
 	
 	@PostMapping(value = "/api/v1/bestiary/{englishName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public BeastDetailApi getBeast(@PathVariable String englishName) {
-		Creature beast = repo.findByEnglishName(englishName.replace('_', ' '));
+		Creature beast = beastRepository.findByEnglishName(englishName.replace('_', ' '));
 		BeastDetailApi beastApi = new BeastDetailApi(beast);
-		Collection<String> images = imageRepo.findAllByTypeAndRefId(ImageType.CREATURE, beast.getId());
+		Collection<String> images = imageRepository.findAllByTypeAndRefId(ImageType.CREATURE, beast.getId());
 		if (!images.isEmpty()) {
 			beastApi.setImages(images);
 		}
@@ -127,7 +143,92 @@ public class BeastApiConroller {
 	@CrossOrigin
 	@GetMapping("/api/fvtt/v1/bestiary")
 	public FBeastiary getCreatures(){
-		List<FBeast> list = ((Collection<Creature>) repo.findAll()).stream().map(FBeast::new).collect(Collectors.toList());
+		List<FBeast> list = ((Collection<Creature>) beastRepository.findAll())
+				.stream()
+				.map(FBeast::new)
+				.collect(Collectors.toList());
 		return new FBeastiary(list);
+	}
+	
+	@PostMapping("/api/v1/filters/bestiary")
+	public FilterApi getFilter() {
+		FilterApi filters = new FilterApi();
+		List<FilterApi> sources = new ArrayList<>();
+		FilterApi spellMainFilter = new FilterApi("main");
+		spellMainFilter.setValues(
+				beastRepository.findBook(TypeBook.OFFICAL).stream()
+				.map(book -> new FilterValueApi(book.getSource(), book.getSource(),	Boolean.TRUE, book.getName()))
+				.collect(Collectors.toList()));
+		sources.add(spellMainFilter);
+		
+		FilterApi settingFilter = new FilterApi("Сеттинги", "settings");
+		settingFilter.setValues(
+				beastRepository.findBook(TypeBook.SETTING).stream()
+				.map(book -> new FilterValueApi(book.getSource(), book.getSource(),	Boolean.TRUE, book.getName()))
+				.collect(Collectors.toList()));
+		sources.add(settingFilter);
+		
+		FilterApi adventureFilter = new FilterApi("Приключения", "adventures");
+		adventureFilter.setValues(
+				beastRepository.findBook(TypeBook.MODULE).stream()
+				.map(book -> new FilterValueApi(book.getSource(), book.getSource(),	Boolean.TRUE, book.getName()))
+				.collect(Collectors.toList()));
+		sources.add(adventureFilter);
+		
+		FilterApi homebrewFilter = new FilterApi("Homebrew", "homebrew");
+		homebrewFilter.setValues(
+				beastRepository.findBook(TypeBook.CUSTOM).stream()
+				.map(book -> new FilterValueApi(book.getSource(), book.getSource(),	Boolean.TRUE, book.getName()))
+				.collect(Collectors.toList()));
+		sources.add(homebrewFilter);
+		filters.setSources(sources);
+		
+		List<FilterApi> otherFilters = new ArrayList<>();
+		
+		FilterApi crFilter = new FilterApi("Уровень опасности", "cr");
+		crFilter.setColapse(Boolean.FALSE);
+		List<FilterValueApi> values = new ArrayList<>();
+		values.add(new FilterValueApi("не определена", "undefined", Boolean.TRUE));
+		values.add(new FilterValueApi("0", "0", Boolean.TRUE));
+		values.add(new FilterValueApi("1/8", "1/8", Boolean.TRUE));
+		values.add(new FilterValueApi("1/4", "1/4", Boolean.TRUE));
+		values.add(new FilterValueApi("1/2", "1/2", Boolean.TRUE));
+		values.addAll(
+				IntStream.rangeClosed(1, 30)
+				.mapToObj(value -> new FilterValueApi(String.valueOf(value), value, Boolean.TRUE))
+				.collect(Collectors.toList()));
+		crFilter.setValues(values);
+		otherFilters.add(crFilter);
+		
+		FilterApi typeFilter = new FilterApi("Тип существа", "type");
+		typeFilter.setValues(
+				CreatureType.getFilterTypes().stream()
+				 .map(value -> new FilterValueApi(value.getCyrilicName(), value.name(), Boolean.TRUE))
+				 .collect(Collectors.toList()));
+		otherFilters.add(typeFilter);
+		
+		FilterApi sizeFilter = new FilterApi("Размер существа", "size");
+		sizeFilter.setValues(
+				CreatureSize.getFilterSizes().stream()
+				 .map(value -> new FilterValueApi(value.getCyrilicName(), value.name(), Boolean.TRUE))
+				 .collect(Collectors.toList()));
+		otherFilters.add(sizeFilter);
+		
+		FilterApi tagFilter = new FilterApi("Тэги", "tag");
+		tagFilter.setValues(
+				tagRepository.findByOrderByName().stream()
+				 .map(value -> new FilterValueApi(value.getName(), value.getId(), Boolean.TRUE))
+				 .collect(Collectors.toList()));
+		otherFilters.add(tagFilter);
+		
+		FilterApi environmentFilter = new FilterApi("Места обитания", "environment");
+		environmentFilter.setValues(
+				HabitatType.types().stream()
+				 .map(value -> new FilterValueApi(value.getName(), value.name(), Boolean.TRUE))
+				 .collect(Collectors.toList()));
+		otherFilters.add(environmentFilter);
+		
+		filters.setOther(otherFilters);
+		return filters;
 	}
 }
