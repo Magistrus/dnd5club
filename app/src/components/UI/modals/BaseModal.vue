39c7fa416f636160a8ad83e414beaee94b29481f
Peterko <peterko@gmail.com>
2022-07-10 13:14:26 +0300

    Fix

--- app/src/components/UI/modals/BaseModal.vue
+++ app/src/components/UI/modals/BaseModal.vue
@@ -172,7 +172,7 @@
             display: flex;
             align-items: center;
             justify-content: space-between;
-            padding: 16px 24px;
+            padding: 16px 16px 16px 24px;
             flex-shrink: 0;
             background-color: var(--hover);
 
