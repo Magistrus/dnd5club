fb221a54397814043b305b49f0e88f65cf7767dd
Magistrus <gm.magistr@gmail.com>
2022-07-13 23:07:05 +0300

    fix random encounters

--- src/main/java/club/dnd5/portal/repository/datatable/RandomEncounterRepository.java
+++ src/main/java/club/dnd5/portal/repository/datatable/RandomEncounterRepository.java
@@ -1,6 +1,7 @@
 package club.dnd5.portal.repository.datatable;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,7 @@ import club.dnd5.portal.model.encounters.RandomEncounterRow;
 
 public interface RandomEncounterRepository extends JpaRepository<RandomEncounterRow, Integer> {
 	@Query("SELECT e FROM RandomEncounterRow e WHERE e.start >= :index AND e.end <= :index AND e.level = :level AND e.type = :type")
-	RandomEncounterRow findOne(
+	Optional<RandomEncounterRow> findOne(
 			@Param("index") int index,
 			@Param("level") int level,
 			@Param("type") HabitatType type);
