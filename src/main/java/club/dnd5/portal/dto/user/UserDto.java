e82be0dfc3d0255b938d5484d9c3d0eefbb69829
Magistrus <gm.magistr@gmail.com>
2022-07-23 23:46:08 +0300

    fix

--- src/main/java/club/dnd5/portal/dto/user/UserDto.java
+++ src/main/java/club/dnd5/portal/dto/user/UserDto.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
 
 @Getter
 @Setter
