b839162b9837e58c6d07ae9de79aa5a325d429cd
Magistrus <gm.magistr@gmail.com>
2022-08-29 09:41:14 +0300

    fix bookmarks api

--- src/main/java/club/dnd5/portal/service/BookmarkServiceImpl.java
+++ src/main/java/club/dnd5/portal/service/BookmarkServiceImpl.java
@@ -1,17 +1,13 @@
 package club.dnd5.portal.service;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import club.dnd5.portal.dto.api.bookmark.BookmarkApi;
-import club.dnd5.portal.model.BookmarkCategory;
 import club.dnd5.portal.model.user.Bookmark;
 import club.dnd5.portal.model.user.User;
 import club.dnd5.portal.repository.user.BookmarkRepository;
@@ -82,60 +78,6 @@ public class BookmarkServiceImpl implements BookmarkService {
 	}
 
 	@Override
-	public void mergeBookmarks(User user, List<BookmarkApi> bookmarksApi) {
-		Optional<Bookmark> rootBookmark = bookmarkRepository.findByUserAndOrder(user , -1);
-		if (rootBookmark.isPresent()) {
-			List<Bookmark> bookmarks = rootBookmark.get().getChildren()
-					.parallelStream()
-					.flatMap(b -> Stream.concat(Stream.of(b), b.getChildren().stream()))
-					.collect(Collectors.toList());
-			bookmarks.add(rootBookmark.get());
-			for (BookmarkApi bookmarkApi : bookmarksApi) {
- 				if (bookmarks.parallelStream().anyMatch(b -> b.getName().equals(bookmarkApi.getName()))) {
-					continue;
-				}
-				Bookmark bookmark = new Bookmark();
-				if (bookmarkRepository.existsById(UUID.fromString(bookmarkApi.getUuid()))) {
-					bookmark.setUuid(getNewUUID());
-				}
-				else {
-					bookmark.setUuid(UUID.fromString(bookmarkApi.getUuid()));
-				}
-				if (bookmarkApi.getParentUUID() != null) {
-					Bookmark parent = bookmarkRepository.getById(UUID.fromString(bookmarkApi.getParentUUID()));
-					bookmark.setParent(parent);
-				}
-				bookmark.setName(bookmarkApi.getName());
-				bookmark.setOrder(bookmarkApi.getOrder());
-				bookmark.setUrl(bookmarkApi.getUrl());
-				bookmark.setPrefix(bookmarkApi.getPrefix());
-				bookmark.setUser(user);
-				bookmarkRepository.save(bookmark);
-			}
-		} else {
-			for (BookmarkApi bookmarkApi : bookmarksApi) {
-				Bookmark bookmark = new Bookmark();
-				if (bookmarkRepository.existsById(UUID.fromString(bookmarkApi.getUuid()))) {
-					bookmark.setUuid(getNewUUID());
-				}
-				else {
-					bookmark.setUuid(UUID.fromString(bookmarkApi.getUuid()));
-				}
-				if (bookmarkApi.getParentUUID() != null) {
-					Bookmark parent = bookmarkRepository.getById(UUID.fromString(bookmarkApi.getParentUUID()));
-					bookmark.setParent(parent);
-				}
-				bookmark.setName(bookmarkApi.getName());
-				bookmark.setOrder(bookmarkApi.getOrder());
-				bookmark.setUrl(bookmarkApi.getUrl());
-				bookmark.setPrefix(bookmarkApi.getPrefix());
-				bookmark.setUser(user);
-				bookmarkRepository.save(bookmark);
-			}
-		}
-	}
-
-	@Override
 	public Collection<BookmarkApi> getRootBookmarks(User user) {
 		return bookmarkRepository.findByUserAndParentIsNull(user)
 				.stream()
@@ -149,17 +91,6 @@ public class BookmarkServiceImpl implements BookmarkService {
 			uuid = getNewUUID();
 		}
 		return uuid;
-	}
-
-	private Bookmark getNewCategory(User user, Bookmark group, Bookmark bookmark) {
-		Bookmark category = new Bookmark();
-		category.setUuid(getNewUUID());
-		category.setName(BookmarkCategory.getCategoryByURL(bookmark.getUrl()).getName());
-		category.setOrder(BookmarkCategory.getCategoryByURL(bookmark.getUrl()).getOrder());
-		category.setParent(group);
-		category.setUser(user);
-
-		return bookmarkRepository.saveAndFlush(category);
 	}
 
 	private Bookmark getUpdatedBookmark(User user, BookmarkApi bookmark) {
