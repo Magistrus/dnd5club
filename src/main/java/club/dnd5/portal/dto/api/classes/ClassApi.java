2d6ea04a00ad4c745e7ba49283e039ef6bfac505
Magistrus <gm.magistr@gmail.com>
2022-09-14 16:19:35 +0300

    class api fix

--- src/main/java/club/dnd5/portal/dto/api/classes/ClassApi.java
+++ src/main/java/club/dnd5/portal/dto/api/classes/ClassApi.java
@@ -1,5 +1,6 @@
 package club.dnd5.portal.dto.api.classes;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import club.dnd5.portal.dto.api.GroupApi;
 import club.dnd5.portal.dto.api.SourceApi;
 import club.dnd5.portal.model.classes.HeroClass;
+import club.dnd5.portal.model.classes.archetype.Archetype;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,6 +30,7 @@ public class ClassApi {
 	private String image;
 	private String icon;
 	private String archetypeName;
+	private boolean sidekick;
 
 	public ClassApi(HeroClass heroClass, ClassRequestApi request) {
 		name = new NameApi(heroClass.getCapitalazeName(), heroClass.getEnglishName());
@@ -39,6 +42,7 @@ public class ClassApi {
 			archetypes = heroClass.getArchetypes().stream()
 					.filter(a -> a.getName().toUpperCase().contains((request.getSearch().getValue().toUpperCase())) || a
 							.getEnglishName().toUpperCase().contains((request.getSearch().getValue().toUpperCase())))
+					.sorted(Comparator.comparing(Archetype::getBook))
 					.map(NamedListApi::new)
 					.collect(Collectors.toList());
 		} else {
@@ -59,6 +63,7 @@ public class ClassApi {
 		if (heroClass.getArchetypeName() != null) {
 			archetypeName = heroClass.getArchetypeName();
 		}
+		sidekick = heroClass.isSidekick();
 		image = String.format("https://image.dnd5.club:8089/classes/background/class-%s.webp", heroClass.getEnglishName().replace(' ', '-').toLowerCase());
 	}
 }
