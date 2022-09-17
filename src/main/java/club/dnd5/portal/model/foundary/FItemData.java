a3b9c0db9b6bf682dd684f45460bfc007ece2288
Magistrus <gm.magistr@gmail.com>
2022-09-18 01:00:37 +0300

    fix export FVTT

--- src/main/java/club/dnd5/portal/model/foundary/FItemData.java
+++ src/main/java/club/dnd5/portal/model/foundary/FItemData.java
@@ -1,5 +1,7 @@
 package club.dnd5.portal.model.foundary;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,6 +36,7 @@ public class FItemData {
     private String chatFlavor = "";
     private String critical;
     private FItemDamage damage;
+    private FCharge recharge;
     private String formula = "";
     private FSave save;
     private FArmor armor;
@@ -60,6 +63,16 @@ public class FItemData {
 	
 	public FItemData(Action action) {
 		name = action.getName();
+		if (name.contains("перезарядка")) {
+			int value = 4;
+			if (name.contains("5")) {
+				value = 5;
+			} else if (name.contains("6")) {
+				value = 5;
+			}
+			recharge = new FCharge();
+			recharge.setValue(value);
+		}
 		description = new FDiscription(action.getDescription().replace("/hero", "http://dnd5.club/hero"));
 		activation = new FActivation(action.getActionType().name().toLowerCase(), (byte) 1, "");
 		duration = new FDuration();
@@ -71,38 +84,79 @@ public class FItemData {
 			actionType = "mwak"; 
 		} else if (action.getDescription().contains("Дальнобойная атака оружием:")) {
 			actionType = "rwak";	
+		} else if (action.getDescription().contains("спасброс")) {
+			actionType = "save";
+			save = new FSave();
+			if (action.getDescription().contains("Силы")) {
+				save.setAbility("str");
+			} else if (action.getDescription().contains("Ловкости")) {
+				save.setAbility("dex");
+			} else if (action.getDescription().contains("Телосложения")) {
+				save.setAbility("con");
+			} else if (action.getDescription().contains("Мудрости")) {
+				save.setAbility("wiz");
+			} else if (action.getDescription().contains("Интеллекта")) {
+				save.setAbility("int");
+			} else if (action.getDescription().contains("Харизмы")) {
+				save.setAbility("cha");
+			}
+			Pattern dcMatcher = 
+					Pattern.compile("(Сл|Сложностью)\\s\\d+");
+			Matcher matcher = dcMatcher.matcher(action.getDescription());
+			if (matcher.find()) {
+				String dc = matcher.group();
+				dc = dc.replaceAll("\\D+", "");
+				save.setDc(Integer.parseInt(dc.trim()));
+				save.setScaling("flat");
+			}
 		}
 		
-		save = new FSave();
 		armor = new FArmor();
 		damage = new FItemDamage();
-		String damageType ="";
-		if (action.getDescription().toLowerCase().contains("колющий урон")) {
-			damageType = "piercing";
-		} else if (action.getDescription().toLowerCase().contains("рубящий урон")) {
-			damageType = "slashing";
-		} else if (action.getDescription().toLowerCase().contains("дробящий урон")) {
-			damageType = "bludgeoning";
-		}else if (action.getDescription().contains("электрич")) {
-			damageType = "lightning";
-		}
 		
-		Pattern pattern = Pattern.compile("\\d+\\s?к\\s?\\d+");
-		Matcher matcher = pattern.matcher(action.getDescription());
+		Queue<String> damageTypes = new LinkedList<>();
+		Pattern patternDamageTypes = 
+				Pattern.compile("колющий урон|рубящий урон|дробящий урон|урон ядом|урон электричеством|урон кислотой|урон огнём");
+		Matcher matcher = patternDamageTypes.matcher(action.getDescription().toLowerCase());
+		while (matcher.find()) {
+			String damageType = matcher.group();
+			switch (damageType) {
+			case "колющий урон":
+				damageTypes.add("piercing");
+				break;
+			case "рубящий урон":
+				damageTypes.add("slashing");
+				break;
+			case "дробящий урон":
+				damageTypes.add("bludgeoning");
+				break;
+			case "урон ядом":
+				damageTypes.add("poison");
+				break;
+			case "урон электричеством":
+				damageTypes.add("lightning");
+				break;
+			case "урон кислотой":
+				damageTypes.add("acid");
+				break;
+			case "урон огнём":
+				damageTypes.add("fire");
+				break;
+				
+			default:
+				break;
+			}
+		}
+
+		Pattern patternDamageFormula = Pattern.compile("\\d+(к|d)\\d+(\\s\\+\\s\\d+){0,}");
+		matcher = patternDamageFormula.matcher(action.getDescription());
 		while (matcher.find()) {
 			String damageFormula = matcher.group().replace("к", "d");
 			if (damage.getParts().isEmpty()) {
-				damage.addDamage(damageFormula + "+@mod", damageType);
+				damage.addDamage(damageFormula, damageTypes.remove());
 			} else {
-				if (action.getDescription().contains("ядом")) {
-					damageType = "poison";
-				} else if (action.getDescription().contains("электрич")) {
-					damageType = "lightning";
-				}
-				else if (action.getDescription().contains("огнем")) {
-					damageType = "fire";
-				}
-				damage.addDamage(damageFormula + "+@mod", damageType);
+				
+				damage.addDamage(damageFormula, damageTypes.remove());
 			}
 		}
 		hp = new FIHP();
